37408cdefe25095c82070b034d4e503d2d1e9e9d
Tomatensuppen <tomiboyeeee@gmail.com>
2026-03-11 00:10:36 +0100

    Update StartScreen.vue

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -112,7 +112,7 @@ const screenStyles = computed(() => ({
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 20px;
+  padding: 8px;
   color: var(--text-primary);
 }
 
