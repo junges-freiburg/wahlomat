ebd522babdd3f389669a0ad250c8b549f0579e30
Tomatensuppen <tomiboyeeee@gmail.com>
2026-03-22 17:04:10 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -430,7 +430,7 @@ body {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-bottom: max(16px, env(safe-area-inset-bottom));
+  padding-bottom: max(30px, env(safe-area-inset-bottom));
   justify-content: space-between;
   overflow: hidden;
   min-height: 0;
